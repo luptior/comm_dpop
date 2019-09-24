2621c1e23744404a67607345441acd353f2ecc42
luptior <luptior@gmail.com>
2019-09-24 16:58:00 -0500

    delete genmerate index and update test.sh

--- test.sh
+++ test.sh
@@ -2,26 +2,30 @@
 PATH="/usr/local/bin/gdate:$PATH"
 
 #sudo sysctl -w net.inet.udp.maxdgram=65535
-script=/Users/luptior/Desktop/Research_3/dpop/src/run.py
-datadir=/Users/luptior/Desktop/Research_3/python_generator/data
+script=/Users/gx/Documents/Research_3/dpop/src/run.py
+datadir=/Users/gx/Documents/Research_3/python_generator/data
 logdir=./log
 mkdir -p $logdir
+
+network=False
+split=True
+
 
 # Define a timestamp function, return seconds.nanoseconds
 timestamp() {
   date +%s
 }
 
-for dom in 20; do
+for dom in 5; do
   for repo in 6; do
       for num in 5; do
              name=random_a${num}_d${dom}_r${repo}
-             log=$logdir/${name}.log
+             log=$logdir/${name}_split${split}_network${network}.log
              timestamp > $log &&
-             echo "start running ${name}" &&
-             python $script --input $datadir/${name}.xml --network False --split False >> $log &&
+             echo "start running ${name} split ${split} network ${network}" &&
+             python $script --input $datadir/${name}.xml --network $network --split $split >> $log &&
              echo "finish running ${name}" &&
-             timestamp >> $log
+             timestamp >> $log &&
              python src/read_log.py $log
         done
     done
