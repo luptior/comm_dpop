214f9031fc0cb82426bbec6fb88428e8f8c8831c
Gan Xu <gan.xu@wustl.edu>
2019-12-22 19:14:35 +0800

    update

---
+++ draw_result.py
@@ -0,0 +1,37 @@
+# This import registers the 3D projection, but is otherwise unused.
+from mpl_toolkits.mplot3d import Axes3D  # noqa: F401 unused import
+
+import matplotlib.pyplot as plt
+from matplotlib import cm
+from matplotlib.ticker import LinearLocator, FormatStrFormatter
+import numpy as np
+import pandas as pd
+
+
+fig = plt.figure()
+ax = fig.gca(projection='3d')
+
+df = pd.read_csv("simulation_result.csv")
+
+# Make data.
+X = [float(x) for x in list(df.columns)[1:]]
+Y = list(df["Unnamed: 0"])
+
+df.set_index("Unnamed: 0")
+X, Y = np.meshgrid(X, Y)
+R = np.sqrt(X**2 + Y**2)
+Z = np.sin(R)
+
+# Plot the surface.
+surf = ax.plot_surface(X, Y, Z, cmap='Greys',
+                       linewidth=0, antialiased=False)
+
+# Customize the z axis.
+ax.set_zlim(-1.01, 1.01)
+ax.zaxis.set_major_locator(LinearLocator(10))
+ax.zaxis.set_major_formatter(FormatStrFormatter('%.02f'))
+
+# Add a color bar which maps values to colors.
+fig.colorbar(surf, shrink=0.5, aspect=5)
+
+plt.show()
--- test.sh
+++ test.sh
@@ -22,17 +22,16 @@ comp_speed=10
 net_speed=20
 comp_set=True
 
-for net_speed in 1 10 50 100 200 500 1000; do
-  for comp_speed in 1 0.5 0.1 0.05 0.025 0.01 0.005; do
-    echo "comp_speed is ${comp_speed}, net_speed os ${net_speed}"
-    for dom in 10; do
-      for repo in 1 2 3 4 5 6 7 8 9 10; do
+for repo in 7; do
+  for net_speed in 1; do
+    for comp_speed in 1 0.5 0.1 0.05 0.025 0.01; do
+      for dom in 30; do
           for num in 5; do
                  mode=list
                  name=random_a${num}_d${dom}_r${repo}
-                 log=$logdir/${name}_${mode}_network${network}.log
+                 log=$logdir/${name}_${mode}_network${net_speed}_comp${comp_speed}.log
                  timestamp > $log &&
-                 echo "start running ${name} mode ${mode} network ${network}" &&
+                 echo "start running ${name} mode ${mode}_network${net_speed}_comp${comp_speed}" &&
                  python $script --input $datadir/${name}.xml \
                                 --network $network \
                                 --mode $mode \
@@ -45,9 +44,9 @@ for net_speed in 1 10 50 100 200 500 1000; do
 
                  mode=split
                  name=random_a${num}_d${dom}_r${repo}
-                 log=$logdir/${name}_${mode}_network${network}.log
+                 log=$logdir/${name}_${mode}_network${net_speed}_comp${comp_speed}.log
                  timestamp > $log &&
-                 echo "start running ${name} mode ${mode} network ${network}" &&
+                 echo "start running ${name} mode ${mode}_network${net_speed}_comp${comp_speed}" &&
                  python $script --input $datadir/${name}.xml \
                                 --network $network \
                                 --mode $mode \
@@ -58,20 +57,20 @@ for net_speed in 1 10 50 100 200 500 1000; do
                  timestamp >> $log &&
                  python src/read_log.py $log
 
-    #             mode=pipeline
-    #             name=random_a${num}_d${dom}_r${repo}
-    #             log=$logdir/${name}_${mode}_network${network}.log
-    #             timestamp > $log &&
-    #             echo "start running ${name} mode ${mode} network ${network}" &&
-    #             python $script --input $datadir/${name}.xml \
-    #                            --network $network \
-    #                            --mode $mode \
-    #                            --computation ${comp_set} \
-    #                            --comp_speed ${comp_speed} \
-    #                            --net_speed ${net_speed} >> $log &&
-    #             echo "finish running ${name}" &&
-#                 timestamp >> $log &&
-#                 python src/read_log.py $log
+                 mode=pipeline
+                 name=random_a${num}_d${dom}_r${repo}
+                 log=$logdir/${name}_${mode}_network${net_speed}_comp${comp_speed}.log
+                 timestamp > $log &&
+                 echo "start running ${name} mode ${mode}_network${net_speed}_comp${comp_speed}" &&
+                 python $script --input $datadir/${name}.xml \
+                                --network $network \
+                                --mode $mode \
+                                --computation ${comp_set} \
+                                --comp_speed ${comp_speed} \
+                                --net_speed ${net_speed} >> $log &&
+                 echo "finish running ${name}" &&
+                 timestamp >> $log &&
+                 python src/read_log.py $log
             done
         done
     done
