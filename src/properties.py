bc131f9242a8e8d3367bc56a90ab668e033b9650
Gan Xu <gan.xu@wustl.edu>
2020-05-05 02:58:23 -0500

    add properties files

---
+++ src/properties.py
@@ -0,0 +1,28 @@
+import json
+import yaml
+import yaml as yaml
+
+
+
+def store_properties(properties: dict, file: str ="../properties.yaml" ):
+    with open(file, "w") as f:
+        yaml.dump(properties, f)
+
+
+def load_properties(file: str ="../properties.yaml" ):
+    properties = {}
+    with open(file, "r") as f:
+        docs = yaml.load_all(f, Loader=yaml.FullLoader)
+        for doc in docs:
+            for k, v in doc.items():
+                properties[k] = v
+    return properties
+
+
+if __name__ == '__main__':
+    properties = {}
+
+    # "UDP", "TCP", "UDP_FEC"
+    properties['network_protocol'] = "UDP_FEC"
+
+    store_properties(properties)
