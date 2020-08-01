b32765aa27da4f0f79d1daec0dd846c61df65d6a
Gan Xu <gan.xu@wustl.edu>
2020-08-01 09:12:04 -0500

    Update network.py
    ser find_free_port to be default on localhoost

--- src/network.py
+++ src/network.py
@@ -19,9 +19,9 @@ from contextlib import closing
 
 
 # a function to find free port on local host
-def find_free_port():
+def find_free_port(ip='localhost'):
     with closing(socket.socket(socket.AF_INET, socket.SOCK_STREAM)) as s:
-        s.bind(('localhost', 0))
+        s.bind((ip, 0))
         s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         return s.getsockname()[1]
 
