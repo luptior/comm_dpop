56727ff2f722b1d320f5bad804291b8e4fbf0e6f
Gan Xu <gan.xu@wustl.edu>
2020-08-06 06:30:04 -0500

    Update agent.py

--- src/agent.py
+++ src/agent.py
@@ -237,7 +237,7 @@ class SplitAgent(Agent):
         while len(self.waiting_ack) > 0:
             time.sleep(0.1)
             self.logger.info(self.waiting_ack)
-        self.logger.info(f"Finished")
+        self.logger.info(f"Finished All Acks:{self.received_ack}")
 
 
 class ListAgent(Agent):
