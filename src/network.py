6681813fae06fb09a3d41c4263700f066e8db0c3
Gan Xu <gan.xu@wustl.edu>
2020-08-14 17:37:56 -0500

    Update network.py
    update comment

--- src/network.py
+++ src/network.py
@@ -25,6 +25,7 @@ def find_free_port(ip='localhost'):
         s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         return s.getsockname()[1]
 
+
 # some constants for network communication
 
 Q = 0.01
@@ -40,6 +41,7 @@ def throughput(agent, q, rtt, s) -> float:
 def tran_time(agent, size):
     # return 0.1 + size / 100 , return unit in s
     return size / throughput(agent, Q, RTT, MAX_SEG)
+
 
 def tcp_rtt(agent, size):
     return size / agent.net_speed * 2
@@ -87,26 +89,34 @@ def proactive(size):
 
 
 def tcp_time_ber(ber: float, bitlength: int) -> float:
-    # to simulate the time spent for tcp with a certain percentage of ber
-    # The TCP checksum is two bytes long
-    # Traces of Internet packets from the past two years show that between 1 packet in 1,100 and 1 packet in 32,000 fails the TCP checksum
+    # to simulate the time spent for tcp with a certain percentage of ber The TCP checksum is two bytes long Traces
+    # of Internet packets from the past two years show that between 1 packet in 1,100 and 1 packet in 32,000
+    # fails the TCP checksum
     if float(ber) == 0:
         return 0
     else:
         return 0
 
 
-def rs_rej_prop(msg_len:int, rs_k: int, ber: float) -> float:
-    # calculate the possibility that a rs coded string to be unrecoverable
-    # msg_len:int, the length of the total message length(original info + RS overhead
-    # rs_k: int, the k vaalue of RS code
-    # ber: float, bit error rate
-    result = sum([comb(msg_len, k) * ber ** k * (1 - ber) ** (msg_len - k) for k in range(0, rs_k+1)])
-    if result > 1 :
+def rs_rej_prop(msg_len: int, rs_k: int, ber: float) -> float:
+    """
+    calculate the possibility that a rs coded string to be unrecoverable
+    :param msg_len: the length of the total message
+    :param rs_k: int, the k value of RS code
+    :param ber: float, bit error rate
+    :return:
+    """
+    result = sum([comb(msg_len, k) * ber ** k * (1 - ber) ** (msg_len - k) for k in range(0, rs_k + 1)])
+    if result > 1:
         return 0
     else:
-        return 1-result
+        return 1 - result
 
 
-def checksum_rej_prop(msg_len:int,  ber: float) -> float:
-    return 1-(1-ber)**msg_len
+def checksum_rej_prop(msg_len: int, ber: float) -> float:
+    """ a function that the package will get rejected by checksum
+    :param msg_len: the length of the total message
+    :param ber: bit error rate
+    :return: a float of possibility it get rejected
+    """
+    return 1 - (1 - ber) ** msg_len
