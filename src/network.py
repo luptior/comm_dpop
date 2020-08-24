817489994ed4e9bdfd99bb0b00927a0fe7f05644
Gan Xu <gan.xu@wustl.edu>
2020-08-24 05:11:23 -0500

    Update network.py

--- src/network.py
+++ src/network.py
@@ -51,48 +51,8 @@ def tran_time(agent, size):
         return size / udp_throughput(Q, RTT, MAX_SEG)
 
 
-def loss(s):
-    # return a loss rate based on size
-    return 1. / (1. + 1. / (s / 10. ** 3))
-
-
-def divide_sleep(size):
-    if np.random.ranf() >= loss(size):
-        # successfully transmitted
-        sleep(tran_time(size))
-    else:
-        # divide and send, timeout 10s
-        sleep(5)
-        divide_sleep(size / 2)
-        divide_sleep(size / 2)
-
-
-def proactive(size):
-    # given a know relationship between size and expectation of delivery time
-    # precalculated the function argmin = 7.9 ,so take 8 here
-    if size <= 8:
-        while np.random.ranf() < loss(size):
-            sleep(5)
-        else:
-            sleep(tran_time(size))
-    else:
-        num = size / 8
-        last = size % 8
-        for i in range(num):
-            # for the packages of size 8
-            while np.random.ranf() < loss(8):
-                sleep(5)
-            else:
-                sleep(tran_time(8))
-        # for the last package
-        if last != 0:
-            while np.random.ranf() < loss(last):
-                sleep(5)
-            else:
-                sleep(tran_time(last))
-
-
 def tcp_time_ber(ber: float, bitlength: int) -> float:
+    # TODO:
     # to simulate the time spent for tcp with a certain percentage of ber The TCP checksum is two bytes long Traces
     # of Internet packets from the past two years show that between 1 packet in 1,100 and 1 packet in 32,000
     # fails the TCP checksum
@@ -108,7 +68,7 @@ def rs_rej_prop(msg_len: int, rs_k: int, ber: float) -> float:
     :param msg_len: the length of the total message
     :param rs_k: int, the k value of RS code
     :param ber: float, bit error rate
-    :return:
+    :return: probability that the message get rejected
     """
     result = sum([comb(msg_len, k) * ber ** k * (1 - ber) ** (msg_len - k) for k in range(0, rs_k + 1)])
     if result > 1:
@@ -124,3 +84,43 @@ def checksum_rej_prop(msg_len: int, ber: float) -> float:
     :return: a float of possibility it get rejected
     """
     return 1 - (1 - ber) ** msg_len
+
+# def loss(s):
+#     # return a loss rate based on size
+#     return 1. / (1. + 1. / (s / 10. ** 3))
+#
+#
+# def divide_sleep(size):
+#     if np.random.ranf() >= loss(size):
+#         # successfully transmitted
+#         sleep(tran_time(size))
+#     else:
+#         # divide and send, timeout 10s
+#         sleep(5)
+#         divide_sleep(size / 2)
+#         divide_sleep(size / 2)
+#
+#
+# def proactive(size):
+#     # given a know relationship between size and expectation of delivery time
+#     # precalculated the function argmin = 7.9 ,so take 8 here
+#     if size <= 8:
+#         while np.random.ranf() < loss(size):
+#             sleep(5)
+#         else:
+#             sleep(tran_time(size))
+#     else:
+#         num = size / 8
+#         last = size % 8
+#         for i in range(num):
+#             # for the packages of size 8
+#             while np.random.ranf() < loss(8):
+#                 sleep(5)
+#             else:
+#                 sleep(tran_time(8))
+#         # for the last package
+#         if last != 0:
+#             while np.random.ranf() < loss(last):
+#                 sleep(5)
+#             else:
+#                 sleep(tran_time(last))
