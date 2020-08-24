1ea641219ef6f10fef0c0f935cc2fedefcc1cc04
Gan Xu <gan.xu@wustl.edu>
2020-08-24 08:54:45 -0500

    change to use pickle

--- src/rs_coding.py
+++ src/rs_coding.py
@@ -1,6 +1,7 @@
 """This module contains utility functions that supports Reed Solomon Coding."""
 
 import logging
+import pickle
 
 from reedsolo import RSCodec, ReedSolomonError
 
@@ -49,81 +50,89 @@ def deserialize(input: bytearray, rsc: RSCodec = RSCodec(10), datatype="int64"):
     combined_decoded = combined.split(b"\tab")
     # [title, shape, actual data]
     title = combined_decoded[0].decode()
-    shape = np.frombuffer(combined_decoded[1], "int64")
+    message = pickle.loads(combined_decoded[1])
+
+    return title, message
+
+    # shape = np.frombuffer(combined_decoded[1], "int64")
     # shape = np.frombuffer(combined_decoded[1].encode(), "int64")
 
-    if title == "ACK":
-        return title, combined_decoded[2].decode("utf-8")
-
-    data = np.frombuffer(combined_decoded[2], datatype)
-
-    if "ptinfo" in title:
-        return title, load_relatives(data)
-    elif "domain_" in title or "neighbors_" in title:
-        return title, list(data)
-    elif "pre_util_msg_" in title:
-        return title, tuple(data)
-    elif "value_msg_" in title:
-        return title, load_dict(data)
-    elif "util_msg_" == title[:9]:
-        # TODO: a more robust way to distinguish these 2
-        if np.prod(shape) == len(data):
-            # pure list no split, product of shape = total elements #
-            if len(shape) == 1:
-                return title, load_dict(data)
-            else:
-                return title, data.reshape(shape)
-        else:
-            # split list, product of shape != total elements #
-            # k = data[:np.prod(shape)]
-            return title, [tuple(shape), list(data)]
-    else:
-        logger.error(f"not supportd input: {title}  {type(data)}")
-        raise Exception(f"not supportd input {title}  {type(data)}")
+    # if title == "ACK":
+    #     return title, combined_decoded[2].decode("utf-8")
+    #
+    # data = np.frombuffer(combined_decoded[2], datatype)
+    #
+    # if "ptinfo" in title:
+    #     return title, load_relatives(data)
+    # elif "domain_" in title or "neighbors_" in title:
+    #     return title, list(data)
+    # elif "pre_util_msg_" in title:
+    #     return title, tuple(data)
+    # elif "value_msg_" in title:
+    #     return title, load_dict(data)
+    # elif "util_msg_" == title[:9]:
+    #     # TODO: a more robust way to distinguish these 2
+    #     if np.prod(shape) == len(data):
+    #         # pure list no split, product of shape = total elements #
+    #         if len(shape) == 1:
+    #             return title, load_dict(data)
+    #         else:
+    #             return title, data.reshape(shape)
+    #     else:
+    #         # split list, product of shape != total elements #
+    #         # k = data[:np.prod(shape)]
+    #         return title, [tuple(shape), list(data)]
+    # else:
+    #     logger.error(f"not supportd input: {title}  {type(data)}")
+    #     raise Exception(f"not supportd input {title}  {type(data)}")
 
 
 def serialize(title: str, message, rsc: RSCodec = RSCodec(10)) -> bytearray:
 
-    if "util_msg_" == title[:9] and isinstance(message, np.ndarray):
-        # original version of util message
-        message = message.astype(int)
-        shape = np.asarray(message.shape, dtype="int64")
-        b = message.tobytes()
-    elif "util_msg_" == title[:9] and isinstance(message, dict):
-        # dict version of the util message, used by pipeline
-        data = dump_dict(message)
-        shape = np.asarray(len(data), dtype="int64")
-        b = np.asarray(data, dtype="int64").tobytes()
-    elif "util_msg_" == title[:9] and isinstance(message[0], tuple):
-        # list version of the util message
-        k = list(message[0])
-        v = list(message[1])
-        shape = np.asarray(k, dtype="int64")
-        b = np.asarray(v, dtype="int64").tobytes()
-    elif "pre_util_msg_" == title[:13] or "neighbors_" in title or "domain_" in title and \
-            (isinstance(message, list) or isinstance(message, tuple)):
-        # for the pre_util_msg_, contain should either be list or tuple
-        shape = np.asarray(len(message))
-        b = np.asarray(message, dtype="int64").tobytes()
-    elif "ptinfo" in title and isinstance(message, datastruct.Relatives):
-        # ptinfo
-        data = dump_relatives(message)
-        shape = np.asarray(len(data), dtype="int64")
-        b = np.asarray(data, dtype="int64").tobytes()
-    elif "value_msg_" in title and isinstance(message, dict):
-        # for value msg, in dict format
-        data = dump_dict(message)
-        shape = np.asarray(len(data), dtype="int64")
-        b = np.asarray(data, dtype="int64").tobytes()
-    elif "ACK" in title:
-        # message should be just str
-        b = data = message.encode("utf-8")
-        shape = np.asarray(len(data), dtype="int64")
-    else:
-        logger.error(f"not supportd input: {title}  {message} {type(message)}")
-        raise Exception(f"not supportd input {title} {type(message)} {message}")
+    # if "util_msg_" == title[:9] and isinstance(message, np.ndarray):
+    #     # original version of util message
+    #     message = message.astype(int)
+    #     shape = np.asarray(message.shape, dtype="int64")
+    #     b = message.tobytes()
+    # elif "util_msg_" == title[:9] and isinstance(message, dict):
+    #     # dict version of the util message, used by pipeline
+    #     # data = dump_dict(message)
+    #     shape = np.asarray(len(message), dtype="int64")
+    #     b = pickle.dumps(message)
+    # elif "util_msg_" == title[:9] and isinstance(message[0], tuple):
+    #     # list version of the util message
+    #     k = list(message[0])
+    #     v = list(message[1])
+    #     shape = np.asarray(k, dtype="int64")
+    #     b = np.asarray(v, dtype="int64").tobytes()
+    # elif "pre_util_msg_" == title[:13] or "neighbors_" in title or "domain_" in title and \
+    #         (isinstance(message, list) or isinstance(message, tuple)):
+    #     # for the pre_util_msg_, contain should either be list or tuple
+    #     shape = np.asarray(len(message))
+    #     b = np.asarray(message, dtype="int64").tobytes()
+    # elif "ptinfo" in title and isinstance(message, datastruct.Relatives):
+    #     # ptinfo
+    #     data = dump_relatives(message)
+    #     shape = np.asarray(len(data), dtype="int64")
+    #     b = np.asarray(data, dtype="int64").tobytes()
+    # elif "value_msg_" in title and isinstance(message, dict):
+    #     # for value msg, in dict format
+    #     data = dump_dict(message)
+    #     shape = np.asarray(len(data), dtype="int64")
+    #     b = np.asarray(data, dtype="int64").tobytes()
+    # elif "ACK" in title:
+    #     # message should be just str
+    #     b = data = message.encode("utf-8")
+    #     shape = np.asarray(len(data), dtype="int64")
+    # else:
+    #     logger.error(f"not supportd input: {title}  {message} {type(message)}")
+    #     raise Exception(f"not supportd input {title} {type(message)} {message}")
 
-    combined_str = title.encode() + b"\tab" + shape.tobytes() + b"\tab" + b
+
+
+    # combined_str = title.encode() + b"\tab" + shape.tobytes() + b"\tab" + b
+
+    combined_str = title.encode() + b"\tab" + pickle.dumps(message)
 
     return rsc.encode(combined_str)
 
@@ -208,9 +217,30 @@ def test_serialize():
     print(equal_arrays)
 
 
-if __name__ == '__main__':
-    test_serialize()
+def test_pickle():
+    rsc = RSCodec(10)
+    shape = (10, 10, 10)
+    input_array = np.random.randint(1000, size=shape)
+    title = "util_msg_1"
 
+    b = pickle.dumps(input_array)
+
+    coded = title.encode() + b"\tab" + b
+
+    print(coded)
+
+    decoded = coded.split(b"\tab")
+
+    print(decoded[0].decode("utf-8"))
+    print(pickle.loads(decoded[1]))
+
+
+
+
+if __name__ == '__main__':
+    # test_serialize()
+
+    test_pickle()
     # serialized = serialize(title="ACK", message="util_msg_1")
     # deserialized = deserialize(serialized)
     #
