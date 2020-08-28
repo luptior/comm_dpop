c5c8e496b3d05f4c426da96c39f83dbee1d0caed
Gan Xu <gan.xu@wustl.edu>
2020-08-28 14:44:02 -0500

    add new funcstions

--- src/optimization.py
+++ src/optimization.py
@@ -5,23 +5,46 @@ import network
 import msg_structure
 
 
-def optimize_size(agent, original_table: np.array, start_length: int = 1) -> int:
+def optimize_size(agent, original_table: np.array, step: int = 1, max_length: int = np.infty) -> int:
     """
     return the size of smaller pieces based on the computation function and tp
+    :param max_length: maximum size of sliced_msgs
     :param agent:
-    :param start_length: a parameter sets the minimum length for the minimizer search
+    :param step: a parameter sets the minimum length for the minimizer search
     :param original_table:
-    :return: a tuple represents the shape
+    :return:  a int length of the suitable size
     """
 
-    if start_length == 1:
+    if step == 1:
         result = [time_with_optimization(agent, original_table, x) for x in
-                  np.arange(start_length, original_table.size+1)]
+                  np.arange(step, min(original_table.size, max_length) + 1)]
     else:
         result = [time_with_optimization(agent, original_table, x)
-                  for x in np.arange(start_length, original_table.size+1, step=start_length)]
+                  for x in np.arange(step, min(original_table.size, max_length) + 1, step=step)]
 
-    length = np.arange(start_length, original_table.size+1, start_length)[np.argmin(result)]
+    length = np.arange(step, min(original_table.size, max_length) + 1, step)[np.argmin(result)]
+
+    return length
+
+
+def optimize_size_dict(agent, msg: dict, step: int = 1, max_length: int = np.infty) -> int:
+    """
+    return the size of smaller pieces based on the computation function and tp
+    :param max_length: maximum size of sliced_msgs
+    :param agent:
+    :param step: a parameter sets the minimum length for the minimizer search
+    :param msg: dict format [(index,), [values]]
+    :return: a int length of the suitable size
+    """
+
+    if step == 1:
+        result = [time_with_optimization_dict(agent, msg, x) for x in
+                  np.arange(step, min(len(msg), max_length) + 1)]
+    else:
+        result = [time_with_optimization_dict(agent, msg, x)
+                  for x in np.arange(step, min(len(msg), max_length) + 1, step=step)]
+
+    length = np.arange(step, min(len(msg), max_length) + 1, step)[np.argmin(result)]
 
     return length
 
@@ -38,6 +61,36 @@ def time_with_optimization(agent, original_table: np.array, length: int) -> floa
     n_pieces = int(np.size(original_table) / length) + 1
     trans = network.tran_time(agent, msg_structure.size_sliced_msg(original_table.shape, length))
     comp = computation_time(agent, msg_structure.size_sliced_msg(original_table.shape, length))
+
+    if trans >= comp:
+        # transmission takes more time
+        return n_pieces * trans + comp
+    else:
+        # computation takes more time
+        return n_pieces * comp + trans
+
+
+def time_with_optimization_dict(agent, msg, length: int) -> float:
+    """
+    calculated the total time spent if apply optimization, there are two conditions, 1, transmission of
+    the msg takes more time, 2, computation takes more time.
+    :param agent:
+    :param msg: {tuple1: value, tuple2: value}
+    :param length:
+    :return: the total time calculated based on the shape of pieces
+    """
+
+    element = {}
+
+    for k, v in enumerate(msg):
+        element = {k: v}
+        break
+
+    element_size = msg_structure.get_actual_size(element)
+
+    n_pieces = int(len(msg) / length) + 1
+    trans = network.tran_time(agent, element_size)
+    comp = computation_time(agent, element_size)
 
     if trans >= comp:
         # transmission takes more time
